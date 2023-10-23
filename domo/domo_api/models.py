a9eceb617c2e96382c7cf99bf0c15f469238c3c5
K-osA <evbayooooo@gmail.com>
2023-10-23 18:30:55 +0900

    fix: modify models
    - When sign-up, created_at value set with current time (UTC)
    
    - Remove explict Token model

--- domo/domo_api/models.py
+++ domo/domo_api/models.py
@@ -1,10 +1,8 @@
-import binascii
-import os
+from datetime import datetime, timezone
 
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
-from domo_base import settings
 
 
 class UserManager(BaseUserManager):
@@ -25,6 +23,7 @@ class UserManager(BaseUserManager):
             name=name,
             short_description=short_description,
             description=description,
+            created_at=datetime.now(tz=timezone.utc),
         )
         if github_link:
             user.github_link = github_link
@@ -69,28 +68,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     created_at = models.DateTimeField()
 
     USERNAME_FIELD = "email"
-
-
-class Token(models.Model):
-    key = models.CharField("Key", max_length=40, primary_key=True)
-    user = models.OneToOneField(
-        settings.AUTH_USER_MODEL,
-        related_name="auth_token",
-        on_delete=models.CASCADE,
-        verbose_name="User",
-    )
-    created = models.DateTimeField("Created", auto_now_add=True)
-
-    class Meta:
-        abstract = "rest_framework.authtoken" not in settings.INSTALLED_APPS
-
-    def save(self, *args, **kwargs):
-        if not self.key:  # self.key에 값이 없으면, 랜덤 문자열을 지정.
-            self.key = self.generate_key()
-        return super().save(*args, **kwargs)
-
-    def generate_key(self):
-        return binascii.hexlify(os.urandom(20)).decode()
-
-    def __str__(self):
-        return self.key
