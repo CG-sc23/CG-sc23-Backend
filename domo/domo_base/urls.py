913e9500c39138c11c4fd21d207cb23c7f68ebb4
K-osA <evbayooooo@gmail.com>
2023-10-26 20:52:48 +0900

    feat: add urls to google oauth

--- domo/domo_base/urls.py
+++ domo/domo_base/urls.py
@@ -16,11 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from domo_api.views import auth, health_check
+from domo_api.views import auth, health_check, social_auth
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", health_check.check, name="health_check"),
+    path(
+        "api/auth/v1/social/google/sign-in",
+        social_auth.Google.sign_in,
+        name="google_sign_in",
+    ),
+    path(
+        "api/auth/v1/social/google/callback/",
+        social_auth.Google.callback,
+        name="google_callback",
+    ),
+    path(
+        "api/auth/v1/social/sign-up", auth.SocialSignUp.as_view(), name="social_sign_up"
+    ),
     path("api/auth/v1/sign-up", auth.SignUp.as_view(), name="sign_up"),
     path("api/auth/v1/sign-in", auth.SignIn.as_view(), name="sign_in"),
     path("api/auth/v1/sign-out", auth.SignOut.as_view(), name="sign_out"),
