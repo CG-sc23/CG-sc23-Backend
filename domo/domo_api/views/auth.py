bd45416ae05d81190038706199978d4eeeccf2aa
K-osA <evbayooooo@gmail.com>
2023-10-26 20:58:46 +0900

    feat: add google oauth & general oauth sign_up

--- domo/domo_api/views/auth.py
+++ domo/domo_api/views/auth.py
@@ -10,6 +10,7 @@ from domo_api.http_model import (
     SignUpRequest,
     SimpleFailResponse,
     SimpleSuccessResponse,
+    SocialSignUpRequest,
 )
 from domo_api.models import User
 from domo_api.s3.profile import ProfileHandler
@@ -18,6 +19,116 @@ from pydantic import ValidationError
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.views import APIView
+
+
+class SocialSignUp(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        name = request.data.get("name")
+        provider = request.data.get("provider")
+        pre_access_token = request.data.get("pre_access_token")
+        github_link = request.data.get("github_link", None)
+        short_description = request.data.get("short_description", None)
+        description = request.data.get("description", None)
+
+        data_dict = {
+            "email": email,
+            "name": name,
+            "provider": provider,
+            "pre_access_token": pre_access_token,
+            "github_link": github_link,
+            "short_description": short_description,
+            "description": description,
+        }
+
+        # validate input
+        try:
+            request_data = SocialSignUpRequest(**data_dict)
+        except ValidationError:
+            return JsonResponse(
+                SimpleFailResponse(
+                    success=False, reason="Invalid request."
+                ).model_dump(),
+                status=400,
+            )
+
+        if not User.objects.filter(email=request_data.email).exists():
+            return JsonResponse(
+                SimpleFailResponse(
+                    success=False, reason="User with this email not exists."
+                ).model_dump(),
+                status=400,
+            )
+
+        user = User.objects.get(email=request_data.email)
+        if user.name != "NOT REGISTERED":
+            return JsonResponse(
+                SimpleFailResponse(
+                    success=False, reason="User with this email already registered."
+                ).model_dump(),
+                status=400,
+            )
+        if user.pre_access_token != request_data.pre_access_token:
+            return JsonResponse(
+                SimpleFailResponse(
+                    success=False, reason="Authentication Failed. (pre_access_token)"
+                ).model_dump(),
+                status=401,
+            )
+
+        # If Upload profile image to S3 first
+        profile_upload_success = False
+        profile_handler = ProfileHandler()
+        if "profile_image" in request.FILES:
+            # Convert image to JPEG
+            uploaded_image = Image.open(request.FILES["profile_image"])
+            output_image_io = io.BytesIO()
+            uploaded_image.convert("RGB").save(
+                output_image_io, format="JPEG", quality=90
+            )
+
+            converted_image_file = io.BytesIO(output_image_io.getvalue())
+
+            profile_upload_success = profile_handler.upload_image(
+                request_data.email, converted_image_file
+            )
+            if not profile_upload_success:
+                return JsonResponse(
+                    SimpleFailResponse(
+                        success=False, reason="Error uploading profile image."
+                    ).model_dump(),
+                    status=500,
+                )
+
+        # Create user
+        try:
+            user.name = request_data.name
+            user.has_profile_image = profile_upload_success
+            user.github_link = (
+                request_data.github_link if request_data.github_link else None
+            )
+            user.short_description = (
+                request_data.short_description
+                if request_data.short_description
+                else None
+            )
+            user.description = (
+                request_data.description if request_data.description else None
+            )
+            user.save()
+        except:
+            if profile_upload_success:
+                profile_handler.delete_directory(request_data.email)
+            return JsonResponse(
+                SimpleFailResponse(
+                    success=False, reason="Error creating user."
+                ).model_dump(),
+                status=500,
+            )
+        return JsonResponse(
+            SimpleSuccessResponse(success=True).model_dump(),
+            status=201,
+        )
 
 
 class SignUp(APIView):
@@ -107,6 +218,7 @@ class SignUp(APIView):
                 description=request_data.description
                 if request_data.description
                 else None,
+                provider="our",
             )
         except:
             if profile_upload_success:
