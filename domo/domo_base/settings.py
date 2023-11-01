48f53bbe32adcadbf6da55dabbddf28bd1364c9d
K-osA <evbayooooo@gmail.com>
2023-11-01 15:46:41 +0900

    fix: allowed_host
    - Add localhost related

--- domo/domo_base/settings.py
+++ domo/domo_base/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.getenv("DOMO_DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["api.domowebest.com", "10.0.11.57"]
+ALLOWED_HOSTS = ["api.domowebest.com", "10.0.11.57", "localhost", "127.0.0.1"]
 
 # Application definition
 
