f801117411450db0c222ea4a0a95529b2f2063ab
K-osA <evbayooooo@gmail.com>
2023-11-23 20:04:45 +0900

    revert: sentry ignore setting
    - now we have 100K sentry quota yeah

--- domo/domo_base/settings/production.py
+++ domo/domo_base/settings/production.py
@@ -1,26 +1,13 @@
 # Sentry
 import sentry_sdk
-from django.core.exceptions import DisallowedHost
 
 from .base import *
-
-
-def before_send(event, hint):
-    if "exc_info" in hint:
-        exc_type, exc_value, tb = hint["exc_info"]
-        if isinstance(exc_value, DisallowedHost):
-            return None
-    return event
-
 
 sentry_sdk.init(
     dsn=os.environ.get("SENTRY_DSN"),
     traces_sample_rate=1.0,
     profiles_sample_rate=1.0,
-    before_send=before_send,
-    before_send_transaction=before_send,
 )
-
 DEBUG = False
 ALLOWED_HOSTS = ["api.domowebest.com", "10.0.11.57"]
 
