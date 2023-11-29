a2ffcda13a3504ea15890b1d41319afc742f24f7
white0825 <white8947@gmail.com>
2023-11-29 18:18:21 +0900

    feat: add get report http model

--- domo/reports/http_model.py
+++ domo/reports/http_model.py
@@ -7,3 +7,9 @@ from pydantic import BaseModel
 class CreateReportRequest(BaseModel):
     title: str
     description: Optional[str] = None
+
+
+class GetAllReportResponse(BaseModel):
+    success: bool
+    count: int
+    reports: list[dict]
