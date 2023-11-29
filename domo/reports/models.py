0707f1e1c2632c3e387a602985d8ee7fded9b8ae
white0825 <white8947@gmail.com>
2023-11-29 17:51:21 +0900

    feat: add report model

---
+++ domo/reports/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from tasks.models import Task
+from users.models import User
+
+
+class Report(models.Model):
+    id = models.AutoField(primary_key=True)
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+    )
+    feed = models.ForeignKey(
+        Task,
+        on_delete=models.CASCADE,
+    )
+
+    title = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    created_at = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
